90255a31d0524b34036ff80c59ea0be15be0c530
rohieminence <rohitthakur.eminence@gmail.com>
2024-01-19 09:44:38 +0530

    debuging with console

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import "hardhat/console.sol";
+
 // Uncomment this line to use console.log
  contract Greeter {
     string private greeting;
-event GreetingChanged(string newGreeting);
     constructor(string memory _greeting){
-         emit GreetingChanged(_greeting);
         greeting = _greeting;
     }
     function greet() public view returns (string memory) {
@@ -14,7 +14,8 @@ event GreetingChanged(string newGreeting);
         
     }
     function setGreeting(string memory _greeting) public {
-        emit GreetingChanged(_greeting);
+                console.log("This greeting will change from %S to %s",greeting,_greeting);   // for debuging 
+
         greeting = _greeting;
     }
 
