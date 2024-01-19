ac4ecc0d4a0378bbe04ddccd2ab615e3543c7454
rohieminence <rohitthakur.eminence@gmail.com>
2024-01-19 09:35:27 +0530

    testing contract

--- test/Sample-test.js
+++ test/Sample-test.js
@@ -1,17 +1,70 @@
-import {expect } from "chai";
-import {ethers} from "hardhat";
+// first step
 
-describe("Greeter",async function(){
-    it("should return the new greeting once it's changed",async function(){
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("Greeter", async function () {
+//   it("Should return the new greeting once it's changed", async function () {
+//     const Greeter = await ethers.getContractFactory("Greeter");
+//     const greeter = await Greeter.deploy("Hello Raja G");
+// await greeter.deployed()  // remove this line not working
+
+//     expect(await greeter.greet()).to.equal("Hello Raja G"); 
+//   });
+// });
+
+
+
+// second 
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("Greeter", async function () {
+//     let contract;
+//     beforeEach(async()=>{
+//         const Greeter = await ethers.getContractFactory("Greeter");
+//         const greeter = await Greeter.deploy("Hello Raja G");
+//         contract = greeter;
+//     })
+//   it("Should return the new greeting once it's changed", async function () {
+   
+
+//     expect(await contract.greet()).to.equal("Hello Raja G");
+   
+//   });
+//   it("Should assign new value to greeting variable " ,async function(){
+//  const setGreetingTx = await contract.setGreeting("Hola,bro what's up");
+
+//     await setGreetingTx.wait();
+
+//     expect(await contract.greet()).to.equal("Hola,bro what's up");
+//   })
+// });
+
+
+
+// third
+
+const { expect,assert } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", async function () {
+    let contract;
+    beforeEach(async()=>{
         const Greeter = await ethers.getContractFactory("Greeter");
         const greeter = await Greeter.deploy("Hello Raja G");
-        await greeter.deployed();
+        contract = greeter;
+    })
+  it("Should return the new greeting once it's changed", async function () {
+   
 
-        expect(await greeter.greet()).to.equal("Hello Raja G");
-        const setGreetingTx = await greeter.setGreeting("Hola,mundoi");
+assert.equal(await contract.greet(),"Hello Raja G")  
+  });
+  it("Should assign new value to greeting variable " ,async function(){
+ const setGreetingTx = await contract.setGreeting("Hola,bro what's up");
 
-        await setGreetingTx.wait();
+    await setGreetingTx.wait();
 
-        expect(await greeter.greet()).to.equal("Hola,mundoi");
-    });
-})
+    expect(await contract.greet()).to.equal("Hola,bro what's up");
+  })
+});
